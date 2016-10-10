b9a0d93a4d16e52005f2b0ada0c3bd7b593bc41a
Carlos Hernandez <learner.cys@gmail.com>
2016-10-10 12:47:52 -0600

    Initial commit: * Basic express server

---
+++ README.md
@@ -0,0 +1,16 @@
+# Beer Locker
+A simple API example based on http://scottksmith.com/blog/2014/05/02/building-restful-apis-with-node/
+
+## Dependencies
+
+* [Nodemon](https://github.com/remy/nodemon)
+
+## Start
+
+```Bash
+// starting the server
+$ npm start
+
+// to start the server using nodemon
+$ npm run start:watch
+```
---
+++ server.js
@@ -0,0 +1,25 @@
+
+// Get the package we need
+var express = require('express');
+
+// Create oru Express application
+var app = express();
+
+// User environement defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+	res.json({ message: 'You are running dangerously low on beer!' });
+});
+
+// Register all our routes wit /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Insert beer on port: ', port);
