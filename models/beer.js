923a8460fd0795c8e005770a570dc75701dc1208
Carlos Hernandez <learner.cys@gmail.com>
2016-10-10 15:59:37 -0600

    * POST /beers * GET  /beers * GET  /beers/:beer_id * PUT  /beers/:beer_id * DELETE /beers/:beer_id

---
+++ models/beer.js
@@ -0,0 +1,12 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our beer schema
+var BeerSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  quantity: Number
+});
+
+// Export the mongoose model
+module.exports = mongoose.model('Beer', BeerSchema);
