941bda9f552647f57dc2a6e63bfb0ec135b3d227
Carlos Hernandez <learner.cys@gmail.com>
2016-10-11 17:04:08 -0600

    Basic Authentication

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-
-// Get the package we need
+// Load required packages
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Beer = require('./models/beer');
+var passport = require('passport');
+var authController = require('./controllers/auth');
+var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
 
 // Connect to the beerlocker MongoDB
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
-// Create oru Express application
+// Create our Express application
 var app = express();
 
 // Use the body-parser package in our application
@@ -16,103 +18,34 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// User environement defined port or 3000
-var port = process.env.PORT || 3000;
+// Use the passport package in our application
+app.use(passport.initialize());
+
 
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' });
-});
+// Create endpoint handlers for /beers
+router.route('/beers')
+  .post(authController.isAuthenticated, beerController.postBeers)
+  .get(authController.isAuthenticated, beerController.getBeers);
 
-var beersRoute = router.route('/beers');
+// Create endpoint handlers for /beers/:beer_id
+router.route('/beers/:beer_id')
+  .get(authController.isAuthenticated, beerController.getBeer)
+  .put(authController.isAuthenticated, beerController.putBeer)
+  .delete(authController.isAuthenticated, beerController.deleteBeer);
 
-// Create endpoint /api/beers for POSTS
-beersRoute.post(function(req, res) {
-  // Create a new instance of the Beer model
-  var beer = new Beer();
+// Create endpoint handlers for /users
+router.route('/users')
+  .post(authController.isAuthenticated, userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers);
 
-  // Set the beer properties that came from the POST data
-  beer.name = req.body.name;
-  beer.type = req.body.type;
-  beer.quantity = req.body.quantity;
-  console.log(beer);
 
-  // Save the beer and check for errors
-  beer.save(function(err) {
-    if (err) {
-      res.send(err);
-    }
-
-    res.json({ message: 'Beer added to the locker!', data: beer });
-  });
-});
-
-beersRoute.get(function(req, res) {
-  // User the Beer model to find all beer
-  Beer.find(function(err, beers) {
-    if(err){
-      res.send(err);
-    }
-
-    res.json(beers);
-  });
-});
-
-// Create a new route with the /bers/:beer_id prefix
-var beerRoute = router.route('/beers/:beer_id');
-
-// Create endpoint /api/beers/:beer_id for GET
-beerRoute.get(function(req, res) {
-  // Use the Beer model to find a specific beer
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if(err){
-      res.send(err);
-    }
-
-    res.json(beer);
-  });
-});
-
-// Create endpoint /api/beers/:beer_id for PUT
-beerRoute.put(function(req, res) {
-  // User the Beer model to find a specific quantity
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if(err) {
-      res.send(err);
-    }
-
-    // Update the existing beer quantity
-    beer.quantity = req.body.quantity;
-
-    // Save the beer and check for errors
-    beer.save(function(err) {
-      if(err) {
-        res.send(err);
-      }
-
-      res.json(beer);
-    });
-  });
-});
-
-beerRoute.delete(function(req, res) {
-  // Use the Beer model to find a specific beer and remove it
-  Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-    if(err) {
-      res.send(err);
-    }
-
-    res.json({ message: 'Beer removed from the locker' });
-  });
-});
-
-// Register all our routes wit /api
+// Register all our routes with /api
 app.use('/api', router);
 
 // Start the server
-app.listen(port);
-console.log('Insert beer on port: ', port);
+app.listen(3000);
+
+console.log('Insert beer on port:  3000');
